abc1afe5d126233bd4b0d18ee7238a816647a2eb
Jade Huynh <35jadeh@gmail.com>
2019-07-27 14:01:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ end
 def apply_clearance(cart)
   cart.each do |item|
     if cart[item][:clearance]
-      cart[item][:price] *= .80
+      cart[item][:price] = (cart[item][:price] * 0.8)
     end
   end
 end
